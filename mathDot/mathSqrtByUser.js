9ea2108e073ee3797405b2028683c6dfdeda8712
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-05-24 20:46:36 +0600

    mathsqrtbyuser

---
+++ mathDot/mathSqrtByUser.js
@@ -0,0 +1,9 @@
+// take the input from the user
+
+const number = prompt("Enter The Number : ");
+
+const result = Math.sqrt(number);
+console.log(`The Square root of ${number} is : ${result}`);
+
+
+
