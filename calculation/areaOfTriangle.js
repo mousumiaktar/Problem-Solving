8d4510d270fb3267af0a3e9c435a143654ec9ea5
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-05-25 18:33:28 +0600

    second formula or triangle

--- calculation/areaOfTriangle.js
+++ calculation/areaOfTriangle.js
@@ -3,8 +3,9 @@
  If you know the base and height of a triangle, you can find the area using the formula:
  area = (base * height) / 2 
  */
-
 //  Calculation Of Tringle Area
+
+
 
 const baseValue = 4;
 const heightValue = 6;
@@ -14,3 +15,23 @@ const areaValue = (baseValue * heightValue) / 2;
 console.log(`The area of the tringle is ${areaValue}`);
 
 
+
+/* If you know all the sides of a triangle, you can find the area using Herons' formula. If a, b and c are the three sides of a triangle, then
+
+s = (a+b+c)/2
+area = √(s(s-a)*(s-b)*(s-c))
+
+*/
+
+
+const side1 = parseInt(3);
+const side2 = parseInt(4);
+const side3 = parseInt(5);
+
+const s = (side1 + side2 + side3) / 2;
+
+const area = Math.sqrt(
+    s * (s-side1) * (s-side2) * (s-side3)
+);
+
+console.log(`The area if the triangle is: ${area}`);
