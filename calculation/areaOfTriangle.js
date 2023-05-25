3d4d1bc508a455e442342f6b3545dd440292ab4f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-05-25 18:21:28 +0600

    triangle area value

---
+++ calculation/areaOfTriangle.js
@@ -0,0 +1,16 @@
+// JavaScript Program to Calculate the Area of a Triangle
+/*
+ If you know the base and height of a triangle, you can find the area using the formula:
+ area = (base * height) / 2 
+ */
+
+//  Calculation Of Tringle Area
+
+const baseValue = 4;
+const heightValue = 6;
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(`The area of the tringle is ${areaValue}`);
+
+
