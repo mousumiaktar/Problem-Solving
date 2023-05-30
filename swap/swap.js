a82ccfcff441f9fadf66f4653abfe6f342518ab8
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-05-30 12:06:02 +0600

    swap by es6

--- swap/swap.js
+++ swap/swap.js
@@ -1,11 +1,11 @@
 let a = 1;
-let b = 2;
+let b= 2;
 
 let result;
-
 result = a;
+
 a = b;
 b = result;
 
-console.log("a=", a);
-console.log("b=", b);
+console.log("a = ", a);
+console.log("b = ", b);
---
+++ swap/swapDestructing.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+
+//using destructuring assignment
+[a, b] = [b, a];
+
+console.log(`The value of a = ${a}`);
+console.log(`The value of b = ${b}`);
